c2d124609ae61422c5583c2d41029a59dbce270c
ndjndj <teq.ndj@gmail.com>
2021-02-25 20:05:15 +0900

    add event

---
+++ main.js
@@ -0,0 +1,53 @@
+const main = () => {
+    //
+
+}
+
+const receiveCSV = () => {
+  const dropZone = document.getElementById('drop-zone');
+  const fileInput = document.getElementById('file-input');
+
+
+  dropZone.addEventListener(
+      'dragover'
+    , function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        this.style.background = '#e1e7f0'
+      }
+    , false
+  );
+
+  dropZone.addEventListener(
+      'dragleave'
+    , function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        this.style.background = '#ffffff'
+      }
+    , false
+  );
+
+  dropZone.addEventListener(
+      'drop'
+    , function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        this.style.background = '#ffffff'
+        let files = e.dataTransfer.files;
+        if (files.length > 1) { return alert('アップロードできるファイルは1つだけです。'); }
+        fileInput.files = files;
+        previewFile(files[0]);
+      }
+    , false
+  );
+
+  fileInput.addEventListener(
+      'change'
+    , function() {
+
+      }
+  );
+
+
+}
