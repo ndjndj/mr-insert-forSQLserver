f31d3d2e915620830eb69971d0656849634aecb5
ndjndj <teq.ndj@gmail.com>
2021-02-25 22:29:00 +0900

    createTable

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ const receiveCSV = () => {
   const dropZone = document.getElementById('drop-zone');
   const fileInput = document.getElementById('file-input');
 
-
   dropZone.addEventListener(
       'dragover'
     , function (e) {
@@ -54,8 +53,6 @@ const receiveCSV = () => {
       previewFile(this.files[0]);
       }
   );
-
-
 }
 
 function previewFile(file) {
@@ -63,7 +60,41 @@ function previewFile(file) {
   fr.readAsText(file);
   fr.onload = function(e) {
     console.log(e.target.result);
+    createTable(e.target.result);
   }
+}
+
+function createTable(filedata) {
+  const arrCSV = filedata.split('\n').map(s => s.split(','));
+
+  const result = document.getElementById('result');
+
+  let table = document.createElement('table');
+  let tmpRow = document.createElement('tr');
+  let tmpElem;
+  let th = document.createElement('th');
+  let td = document.createElement('td');
+  for (var i = 0; i < arrCSV.length; i++) {
+    for (var j = 0; j < arrCSV[i].length; j++) {
+      tmpElem = i == 0 ? th : td;
+      console.log(arrCSV[i][j]);
+      tmpElem.innerText = arrCSV[i][j];
+      tmpRow.appendChild(tmpElem);
+
+      // initialize
+      th = null;
+      td = null;
+      th = document.createElement('th');
+      td = document.createElement('td');
+    }
+
+    table.appendChild(tmpRow);
+    tmpRow = null;
+    tmpRow = document.createElement('tr');
+  }
+
+  result.appendChild(table);
+
 }
 
 main();
